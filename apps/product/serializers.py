f085d7e94d073b6a7cf765afd7f121fac5a134d1
77pozitiv777-dev <77pozitiv777@gmail.com>
2026-02-04 16:23:07 +0600

    Third lesson

--- apps/product/serializers.py
+++ apps/product/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
+
 from apps.product.models import Category, Models, Product, ProductImage
+from lesson_1.apps import product
 
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
-        fields = ['id', 'image']
+        fields = ['image']
 
 class ProductSerializer(serializers.ModelSerializer):
     first_image = serializers.SerializerMethodField()
@@ -28,19 +30,64 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     images = ProductImageSerializer(many=True, read_only=True)
-    category = serializers.StringRelatedField()
+    category_title = serializers.CharField(source='category.title', read_only=True)
+    model_title = serializers.CharField(source='model.title', read_only=True)
 
     class Meta:
         model = Product
         fields = [
-            "id",
-            "uuid",
-            "title",
-            "description",
-            "price",
-            "images",
-            "category"
+            "id", "uuid", "title", 
+            "description", "price", 
+            "created_at", "size", 
+            "is_active", "is_favorite", 
+            "images", "model_title", "category_title"
         ]
+
+class ProductCreateSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(max_length=1000000, allow_empty_file=False, use_url=False),
+        write_only=True,
+        required=False
+    )
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'category', 'model', 'title', 'description', 
+            'price', 'size', 'images', 'uploaded_images'
+        ]
+
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Название должно быть минимум 3 символа!")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена должно быть больше 0!")
+        return value
+
+    def validate_size(self, value):
+        if len(value) > 10:
+            raise serializers.ValidationError("Размер слишком длинный!")
+        return value
+
+    def validate(self, attrs):
+        category = attrs.get("category")
+        model = attrs.get("model")
+
+        if model and category and model.category != category:
+            raise serializers.ValidationError(
+                "Модель не принадлежит выбранной категории!"
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop('uploaded_images', [])
+        product = Product.objects.create(**validated_data)
+        for image in uploaded_images:
+            ProductImage.objects.create(product=product, image=image)
+
+        return product
